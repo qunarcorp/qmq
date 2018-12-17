88e7ecff6aba6c7cd3d22fa69991337f6dc4bf60
keli.wang <keli.wang@qunar.com>
2018-12-17 16:03:54 +0800

    fix license description

--- qmq-delay-server/src/main/java/qunar/tc/qmq/delay/store/model/AppendDispatchRecordResult.java
+++ qmq-delay-server/src/main/java/qunar/tc/qmq/delay/store/model/AppendDispatchRecordResult.java
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.com.qunar.pay.trade.api.card.service.usercard.UserCardQueryFacade
+ * limitations under the License.
  */
 
 package qunar.tc.qmq.delay.store.model;
