86b365ca1e0dc8b76a66a53cfdf1acc2536d41e0
zh_yu <zh_yu@ctrip.com>
2019-01-29 17:11:26 +0800

    test page

--- qmq-demo/src/main/java/qunar/tc/qmq/demo/config/JpaConfiguration.java
+++ qmq-demo/src/main/java/qunar/tc/qmq/demo/config/JpaConfiguration.java
@@ -16,16 +16,10 @@
 
 package qunar.tc.qmq.demo.config;
 
-import org.hibernate.jpa.boot.spi.EntityManagerFactoryBuilder;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
