af8560b672cf12097fa9f5338b28b07780b769f1
keli.wang <keli.wang@qunar.com>
2019-06-10 14:40:07 +0800

    add MessageMemTable class for store message in memory

--- qmq-store/src/main/java/qunar/tc/qmq/store/GetMessageStatus.java
+++ qmq-store/src/main/java/qunar/tc/qmq/store/GetMessageStatus.java
@@ -22,12 +22,13 @@ package qunar.tc.qmq.store;
  */
 public enum GetMessageStatus {
     SUCCESS,
-
     OFFSET_OVERFLOW,
-
     NO_MESSAGE,
-
     SUBJECT_NOT_FOUND,
+    EMPTY_CONSUMER_LOG,
 
-    EMPTY_CONSUMER_LOG
+
+    SEQUENCE_TOO_SMALL,
+    SEQUENCE_TOO_LARGE,
+    TABLE_ALREADY_EVICTED,
 }
