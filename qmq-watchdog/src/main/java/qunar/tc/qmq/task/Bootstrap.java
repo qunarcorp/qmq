b8b1dafdc2097e91c6862ec9529d4411b89d7266
zhaohui.yu <zhaohui.yu@bianlifeng.com>
2019-06-24 17:12:24 +0800

    [zhaohui.yu] provide SPI for access transactional DB

--- qmq-watchdog/src/main/java/qunar/tc/qmq/task/Bootstrap.java
+++ qmq-watchdog/src/main/java/qunar/tc/qmq/task/Bootstrap.java
@@ -16,24 +16,22 @@
 
 package qunar.tc.qmq.task;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.concurrent.CountDownLatch;
+
+import javax.sql.DataSource;
+
 import qunar.tc.qmq.MessageProducer;
 import qunar.tc.qmq.configuration.DynamicConfig;
 import qunar.tc.qmq.configuration.DynamicConfigLoader;
 import qunar.tc.qmq.producer.MessageProducerProvider;
 import qunar.tc.qmq.task.database.DatabaseDriverMapping;
-import qunar.tc.qmq.task.database.IDatabaseDriver;
-import qunar.tc.qmq.task.database.MysqlMMMDatabaseDriver;
 import qunar.tc.qmq.task.database.TomcatDataSourceService;
 import qunar.tc.qmq.task.store.IDataSourceConfigStore;
 import qunar.tc.qmq.task.store.impl.CachedMessageClientStore;
 import qunar.tc.qmq.task.store.impl.DataSourceConfigStoreImpl;
 import qunar.tc.qmq.task.store.impl.LeaderElectionDaoImpl;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Bootstrap {
     public static void main(String[] args) throws InterruptedException {
@@ -64,9 +62,9 @@ public class Bootstrap {
     }
 
     private static DatabaseDriverMapping initDriverMapping() {
-        Map<String, IDatabaseDriver> mapping = new HashMap<>();
-        mapping.put("mmm", new MysqlMMMDatabaseDriver());
-        return new DatabaseDriverMapping(mapping);
+        DatabaseDriverMapping databaseDriverMapping = new DatabaseDriverMapping();
+        databaseDriverMapping.init();
+        return databaseDriverMapping;
     }
 
     private static JdbcTemplate createJdbcTemplate() {
