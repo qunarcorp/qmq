f9b07f83947baa1c6fda0d749e2af66645857bb2
zhaohui.yu <zhaohui.yu@bianlifeng.com>
2020-06-09 12:21:06 +0800

    add License

--- qmq-store/src/main/java/qunar/tc/qmq/store/PullLogMemTable.java
+++ qmq-store/src/main/java/qunar/tc/qmq/store/PullLogMemTable.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Qunar, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package qunar.tc.qmq.store;
 
 import io.netty.buffer.ByteBuf;
@@ -24,11 +40,6 @@ public class PullLogMemTable extends MemTable {
 
     public PullLogMemTable(final long tabletId, final long beginOffset, final int capacity) {
         super(tabletId, beginOffset, capacity);
-    }
-
-    @Override
-    public int getTotalDataSize() {
-        return 0;
     }
 
     @Override
@@ -73,6 +84,11 @@ public class PullLogMemTable extends MemTable {
     @Override
     public void close() {
 
+    }
+
+    @Override
+    public int getTotalDataSize() {
+        return 0;
     }
 
     private static class PullLogSequence {
